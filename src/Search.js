d05299e7d7c0e0ad5942e1400cb68c3529890bef
hibaAkroush <hiba.akr@gmail.com>
2018-01-05 12:28:28 +0200

    search bar

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,7 @@ import fb from "./web/facebook-3x.png"
 import tw from "./web/twitter-3x.png"
 import insta from "./web/instagram-3x.png"
 import SearchButton from "./web/1.png"
-
+import dd from "./web/dd.png"
 class Search extends Component {
   render() {
     return (
@@ -13,9 +13,11 @@ class Search extends Component {
 			<input id="input"></input>
 			<p id="value">Search by event name</p> 
       <img id="searchButton" src={SearchButton}/>
-			<select id="choose"></select>
+			<button id="choose"></button>
       <p id="textChoose">Choose City</p>
-			<select id="week"></select>
+      <img src={dd} className="dropdownmenu1"/>
+			<button  id="week"></button>
+      <img src={dd} className="dropdownmenu2"/>
       <p id="textWeek">This Week</p>
       <img id="tw" src={tw}/> 
     	<img id="fb" src={fb}/> 
